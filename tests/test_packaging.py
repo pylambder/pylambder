2e86a2942be33a673a0e515f6918701f25c6838b
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-09-08 20:38:23 +0200

    Improve code style in tests

--- tests/test_packaging.py
+++ tests/test_packaging.py
@@ -6,13 +6,12 @@ import tempfile
 
 import pylambder.packaging as packaging
 
+
 class TestPackaging(unittest.TestCase):
 
     def test_archive_creation(self):
         def pip_mock(command):
-            self.assertEqual('pip', command[0])
-            self.assertEqual('install', command[1])
-            self.assertEqual('-t', command[2])
+            self.assertEqual(['pip', 'install', '-t'],  command[0:3])
             dir = command[3]
             packages = command[4:]
 
@@ -22,8 +21,7 @@ class TestPackaging(unittest.TestCase):
                 with open(os.path.join(package_dir, "stub"), "w") as f:
                     f.write("mocked file content\n")
 
-        with patch('subprocess.check_call') as mock_call:
-            mock_call.side_effect = pip_mock
+        with patch('subprocess.check_call', side_effect=pip_mock) as mock_call:
 
             with tempfile.TemporaryDirectory() as dir:
                 archive_path = os.path.join(dir, "archive")
@@ -35,11 +33,15 @@ class TestPackaging(unittest.TestCase):
 
                 zip = zipfile.ZipFile(archive_path_with_ext)
 
-                self.assertTrue(_is_name_in_zip("python/lib/python3.7/site-packages/somepackage/", zip))
-                self.assertTrue(_is_name_in_zip("python/lib/python3.7/site-packages/somepackage/stub", zip))
-                self.assertTrue(_is_name_in_zip("python/lib/python3.7/site-packages/otherpackage/", zip))
-                self.assertTrue(_is_name_in_zip("python/lib/python3.7/site-packages/otherpackage/stub", zip))
+                self.assertTrue(_is_name_in_zip(
+                    "python/lib/python3.7/site-packages/somepackage/", zip))
+                self.assertTrue(_is_name_in_zip(
+                    "python/lib/python3.7/site-packages/somepackage/stub", zip))
+                self.assertTrue(_is_name_in_zip(
+                    "python/lib/python3.7/site-packages/otherpackage/", zip))
+                self.assertTrue(_is_name_in_zip(
+                    "python/lib/python3.7/site-packages/otherpackage/stub", zip))
 
 
 def _is_name_in_zip(name: str, zip_file: zipfile.ZipFile) -> bool:
-        return name in zip_file.namelist()
+    return name in zip_file.namelist()
