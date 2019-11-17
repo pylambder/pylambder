43ca68f8de4b7ea87d00dd006b0ea6a5a052cd55
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-11-17 22:08:11 +0100

    Conditionally upload missing zips for the changeset

--- sam-app/deployment_script.py
+++ sam-app/deployment_script.py
@@ -1,12 +1,20 @@
 import time
 import boto3
 import sys
+import hashlib
+from typing import List
+from botocore.exceptions import ClientError
 from pathlib import Path
 
 client = boto3.client('cloudformation')
 s3_client = boto3.client('s3')
+s3_resource = boto3.resource('s3')
+
+Uri = str
 
 BUCKET = 'wgeisler-sam'
+# TODO user version in the prefix or in object metadata to allow easier removal of outdated code
+URI_PREFIX = 'pylambder/'
 stackname = "new-stack"
 templateurl = ""
 params = []
@@ -23,30 +31,67 @@ def changeSetStatus(change_set_name, client):
         return response['Status']
 
 
-def upload_functions():
+def file_md5sum(path):
+    archive = open(path, 'rb').read()
+    return str(hashlib.md5(archive).hexdigest())
+
+
+def upload_if_missing(path) -> Uri:
+    # @TODO handle missing local file
+    local_md5 = file_md5sum(path)
+    obj_key = URI_PREFIX + local_md5
+    needs_upload = True
+    try:
+        # Asssume etag is the file's md5 - true unless
+        # the file is over 5GiB or uploaded as multipar
+
+        obj = s3_resource.Object(BUCKET, obj_key)
+        etag = obj.e_tag.strip('"') # e_tag from boto3 is wrapped in quotes
+        print('etag:', etag, etag.__class__)
+        print('local md5:', local_md5, local_md5.__class__)
+        needs_upload = etag != local_md5
+        print("needs upload:", needs_upload)
+    except Exception as ex:
+        # ignore errors about (maybe) existing object, reupload
+        pass
+
+    if needs_upload:
+        print("Uploading {} as {}".format(path, obj_key))
+        s3_client.upload_file(str(path), BUCKET, obj_key)
+    else:
+        print("Skipping upload of {}: already exists as {}".format(path, local_md5))
+    return F's3://{BUCKET}/{obj_key}'
+
+
+
+
+
+
+def upload_functions() -> List[Uri]:
     """Expects functions zips to exist"""
+    uris = dict()
     for fun in function_names:
         file_name = F'{fun}.zip'
         print("Uploading {} to s3".format(file_name))
-        s3_client.upload_file(str(file_name), BUCKET, file_name)
+        uris[fun] = upload_if_missing(file_name)
+    return uris
 
-def upload_layers():
-    # @TODO: reuse existing
+def upload_layers() -> List[Uri]:
+    uris = dict()
     for layer in layer_names:
         file_name = F'{layer}.zip'
         file_path = Path('lambda-layers') / file_name
         print("Uploading {} to s3".format(file_name))
-        s3_client.upload_file(str(file_path), BUCKET, file_name)
+        uris[layer] = upload_if_missing(str(file_path))
+    return uris
 
 
 def main():
     # Create change set
     print("Creating change set...")
 
-    upload_functions()
-    upload_layers()
-
-    uris = {name: F's3://{BUCKET}/{name}.zip' for name in function_names + layer_names}
+    uris = upload_functions()
+    uris.update(upload_layers())
     print(uris)
     template_body = template_body_format_str.format(**uris)
     print(template_body)
@@ -55,7 +100,7 @@ def main():
         TemplateBody=template_body,
         Parameters=params,
         Capabilities=[capabilities],
-        ChangeSetType="CREATE",
+        ChangeSetType="CREATE", # TODO check if stack exists and use UPDATE
         ChangeSetName=stackname + "-cs"
     )
 
