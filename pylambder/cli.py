64296f98dd381cc37f03707452d764b4e9270b0a
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-11-18 23:04:20 +0100

    Improve pylambder CLI help output

--- pylambder/cli.py
+++ pylambder/cli.py
@@ -1,4 +1,6 @@
 
 
 def main():
-    print("The CLI pylambder interface")
+    print("The CLI pylambder interface.")
+    print("Available commands:\n")
+    print("{:<10s} Upload the pylambder and user code to AWS".format('deploy'))
