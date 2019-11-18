deca9df48e005c8d691affd7884ea0e3e3c20550
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-11-18 23:45:47 +0100

    Add function for zip-packaging selected project directories

--- pylambder/packaging/packaging.py
+++ pylambder/packaging/packaging.py
@@ -4,9 +4,13 @@ import os
 import shutil
 import subprocess
 import tempfile
-from typing import List
+import zipfile
+from pathlib import Path
+from typing import List, Union
 
 from pylambder import APP_NAME
+
+PathOrString = Union[Path, str]
 
 
 def create_packages_archive(target_path: str, package_specs: List[str]) -> None:
@@ -29,5 +33,31 @@ def create_packages_archive(target_path: str, package_specs: List[str]) -> None:
         pip_command = ['pip', 'install', '-t', installation_dir] + package_specs
         subprocess.check_call(pip_command)
 
-        # if file exists at target_path.zip it is overwritten
-        shutil.make_archive(target_path, format='zip', root_dir=tempdir, base_dir='.')
+        with zipfile.ZipFile(target_path, mode='w') as zf:
+            _recursive_zip_write(zf, Path(tempdir), Path(tempdir))
+
+
+def create_project_archive(target_path: PathOrString, base_path: PathOrString,
+                           project_dirs: PathOrString):
+    """Obtains specified packages and puts them in a zip file
+    ready to by uploaded to AWS
+
+    :param target_path: path for the archive. Should contain the ".zip"
+        suffix
+    :param base_path: common root for the packaged files
+    :param project_dirs: list of directories to put in the archive
+    """
+    base_path = Path(base_path)
+    with zipfile.ZipFile(target_path, mode='w') as zf:
+        for dir in project_dirs:
+            _recursive_zip_write(zf, Path(base_path), Path(dir))
+
+
+def _recursive_zip_write(zf: zipfile.ZipFile, relative_to: Path, dir: Path):
+    for dirpath, subdirs, subfiles in os.walk(relative_to / dir):
+        dirpath = Path(dirpath)
+
+        for subdir in sorted(subdirs + subfiles):
+            full_path = dirpath / subdir
+            packaged_path = full_path.relative_to(relative_to)
+            zf.write(full_path, packaged_path)
