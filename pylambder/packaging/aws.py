009465c6755ecb537ac7fc9d007dd97dc2189dd0
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-11-23 14:09:34 +0100

    Deploy application using CLI

--- pylambder/packaging/aws.py
+++ pylambder/packaging/aws.py
@@ -1,5 +1,6 @@
 import hashlib
 import logging
+import time
 from pathlib import Path
 from typing import Union
 
@@ -44,12 +45,79 @@ def upload_bytes_if_missing(bucket_name: str, file_bytes) -> Uri:
     return f"s3://{bucket_name}/{obj_key}"
 
 
+def create_change_set(stack_name, template, change_set_name) -> str:
+    """Returns False if no changes are to be made, True otherwise"""
+    operation = 'UPDATE' if _stack_exists(stack_name) else 'CREATE'
+
+    cs_response = cf_client.create_change_set(
+        StackName=stack_name,
+        TemplateBody=template,
+        Parameters=[],
+        Capabilities=['CAPABILITY_IAM'],
+        ChangeSetType=operation,
+        ChangeSetName=change_set_name
+    )
+    logger.debug('Create change set result: %s', cs_response)
+    change_set_arn = cs_response['Id']
+    return change_set_arn
+
+
+def execute_changeset(stack_name, chane_set_name):
+    ex_response = cf_client.execute_change_set(
+        ChangeSetName=change_set_name
+    )
+    logger.debug("Execute change set reponse: {}".format(ex_response))
+    wait_for_stack_update(stack_name)
+
+
+def wait_for_change_set_creation(change_set_arn):
+    """Returns False if no changes are to be made, True otherwise"""
+    response = cf_client.describe_change_set(
+        ChangeSetName=change_set_arn,
+    )
+    status, reason = response['Status'], response.get('StatusReason', '')
+    logger.debug("Change set status: %s %s", status, reason)
+    if response == 'CREATE_COMPLETE':
+        return True
+    if response == 'FAILED':
+        if reason == 'No updates are to be performed.':
+            return False
+        else:
+            # TODO use pylambder exception
+            raise RuntimeError('Change set creation failed')
+    time.sleep(5)
+
+
+def wait_for_stack_update(stack_name):
+    stack = cf_resource.Stack(stack_name)
+    while True:
+        stack.reload()
+        logger.debug(f"Stack '%s' status: %s", stack_name, stack.stack_status)
+        if stack.stack_status not in ['CREATE_IN_PROGRESS', 'UPDATE_IN_PROGRESS']:
+            break
+        time.sleep(5)
+    
+
 def _s3_object_exists(bucket_name, key) -> bool:
     try:
         s3_resource.Object(bucket_name, key).load()
         return True
     except ClientError:
         return False
+
+
+def _stack_exists(name) -> bool:
+    try:
+        stack = cf_resource.Stack(name)
+
+        # Empty stack with state REVIEW_IN_PROGRESS appears when change set is
+        # created. It is not deemed existing for the purposes of CREATE/UPDATE
+        # changeset distinction.
+        return stack.stack_status != 'REVIEW_IN_PROGRESS'
+    except ClientError as ex:
+        print("Client error: ", ex)
+        return False
+
 
 
 def _file_md5sum(path: Path) -> str:
--- pylambder/packaging/deployment.py
+++ pylambder/packaging/deployment.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import pkg_resources
+import datetime
 from pathlib import Path
 
 import pylambder.config as config
@@ -36,7 +37,8 @@ def deploy(application_dir='.'):
     os.makedirs(application_dir / ARTIFACTS_DIR, exist_ok=True)
 
     package(application_dir)
-    _upload(application_dir, config.get('s3bucket'))
+    _deploy(application_dir, config.get('s3bucket'), config.get('cloudformation_stack'))
+    logger.info("AWS deployment finished.")
 
 
 def _package(application_dir: Path):
@@ -61,22 +63,36 @@ def _get_deps_list(application_dir: Path) -> [str]:
         []
 
 
-def _upload(application_dir: Path, s3_bucket: str):
+def _deploy(application_dir: Path, s3_bucket: str, stack_name: str):
     uris = _upload_pylambder(s3_bucket)
     uris.update(_upload_project(s3_bucket))
+    template = format_template(uris)
+    change_set_name = F"{stack_name}-{datetime.datetime.now().strftime('%Y%m%dT%H%M%S')}"
+    change_set_arn = aws.create_change_set(stack_name, template, change_set_name)
+    if not aws.wait_for_change_set_creation(change_set_arn):
+        logger.info("Stack exists, no changes required")
+        return
+    aws.execute_changeset(stack_name, change_set_arn)
 
 
 def _upload_pylambder(bucket_name: str):
     """Upload pylambder functions"""
-    uris={}
+    uris = {}
     for fun in FUNCTION_NAMES:
-        zipbytes=pkg_resources.resource_string('pylambder', f'packaged/{fun}.zip')
-        uris[fun]=aws.upload_bytes_if_missing(bucket_name, zipbytes)
+        zip_bytes = pkg_resources.resource_string('pylambder', f'packaged/{fun}.zip')
+        uris[fun] = aws.upload_bytes_if_missing(bucket_name, zip_bytes)
     return uris
+
 
 def _upload_project(bucket_name: str):
     """Upload pylambder functions"""
-    uris={}
+    uris = {}
     for layer, archive in LAYERS.items():
-        uris[layer]=aws.upload_file_if_missing(bucket_name, archive)
+        uris[layer] = aws.upload_file_if_missing(bucket_name, archive)
     return uris
+
+
+def format_template(uris: dict):
+    template_body_format_str = pkg_resources.resource_string(
+        'pylambder', 'sam-data/template.yaml.template').decode()
+    return template_body_format_str.format(**uris)
