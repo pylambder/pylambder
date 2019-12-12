7b58c650e7ae9c7bff944c28791841569148d2ee
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-12-12 01:45:19 +0100

    Use AWS credentials defined in pylambder_config
    Resolves #5

--- pylambder/config.py
+++ pylambder/config.py
@@ -14,23 +14,36 @@ class MissingConfig(Exception):
         self.errors = errors
 
 
+loaded = False
 S3BUCKET = None
 CLOUDFORMATION_STACK = None
 API_TOKEN = None
+AWS_ACCESS_KEY_ID = None
+AWS_ACCESS_KEY_SECRET = None
 
 
-def load_config():
+def ensure_loaded() -> None:
+    if not loaded:
+        load_config()
+
+
+def load_config() -> None:
     sys.path.insert(0, os.getcwd())
     try:
         import pylambder_config
 
+        global loaded
         global S3BUCKET
         global CLOUDFORMATION_STACK
         global API_TOKEN
+        global AWS_ACCESS_KEY_ID
+        global AWS_ACCESS_KEY_SECRET
     
         S3BUCKET = pylambder_config.s3bucket
         CLOUDFORMATION_STACK = pylambder_config.cloudformation_stack
         API_TOKEN = pylambder_config.api_token
+        AWS_ACCESS_KEY_ID = pylambder_config.aws_access_key_id
+        AWS_ACCESS_KEY_SECRET = pylambder_config.aws_access_key_secret
 
         if not S3BUCKET:
             raise MissingConfig('Pylambder config variable "s3bucket" is empty')
@@ -38,6 +51,12 @@ def load_config():
             raise MissingConfig('Pylambder config variable "cloudformation_stack" is empty')
         if not API_TOKEN:
             raise MissingConfig('Pylambder config variable "api_token" is empty')
+        if not AWS_ACCESS_KEY_ID:
+            raise MissingConfig('Pylambder config variable "aws_access_key_id" is empty')
+        if not AWS_ACCESS_KEY_SECRET:
+            raise MissingConfig('Pylambder config variable "aws_access_key_secret" is empty')
+
+        loaded = True
 
     except AttributeError as attribute_err:
         raise MissingConfig("Some values are missing in your pylambder config file.", attribute_err) from None
--- pylambder/packaging/aws.py
+++ pylambder/packaging/aws.py
@@ -1,16 +1,12 @@
-import hashlib
 import logging
+import hashlib
 import time
 from pathlib import Path
 from typing import Union
 
 import boto3
+from pylambder import config
 from botocore.exceptions import ClientError
-
-cf_client = boto3.client("cloudformation")
-cf_resource = boto3.resource("cloudformation")
-s3_client = boto3.client("s3")
-s3_resource = boto3.resource("s3")
 
 PathOrString = Union[Path, str]
 Uri = str
@@ -39,7 +35,7 @@ def upload_bytes_if_missing(bucket_name: str, file_bytes) -> Uri:
     if needs_upload:
         logger.info("Uploading file as {}".format(obj_key))
         # TODO Add ContentMD5: base64(md5)
-        s3_client.put_object(Bucket=bucket_name, Body=file_bytes, Key=obj_key)
+        _get_boto_client("s3").put_object(Bucket=bucket_name, Body=file_bytes, Key=obj_key)
     else:
         logger.debug("Skipping upload: already exists as {}".format(obj_key))
     return f"s3://{bucket_name}/{obj_key}"
@@ -49,7 +45,7 @@ def create_change_set(stack_name, template, change_set_name) -> str:
     """Returns False if no changes are to be made, True otherwise"""
     operation = 'UPDATE' if _stack_exists(stack_name) else 'CREATE'
 
-    cs_response = cf_client.create_change_set(
+    cs_response = _get_boto_client("cloudformation").create_change_set(
         StackName=stack_name,
         TemplateBody=template,
         Parameters=[],
@@ -63,7 +59,7 @@ def create_change_set(stack_name, template, change_set_name) -> str:
 
 
 def execute_changeset(stack_name, change_set_arn) -> bool:
-    ex_response = cf_client.execute_change_set(
+    ex_response = _get_boto_client("cloudformation").execute_change_set(
         ChangeSetName=change_set_arn
     )
     logger.debug("Execute change set reponse: {}".format(ex_response))
@@ -73,7 +69,7 @@ def execute_changeset(stack_name, change_set_arn) -> bool:
 def wait_for_change_set_creation(change_set_arn):
     """Returns False if no changes are to be made, True otherwise"""
     while True:
-        response = cf_client.describe_change_set(
+        response = _get_boto_client("cloudformation").describe_change_set(
             ChangeSetName=change_set_arn,
         )
         status, reason = response['Status'], response.get('StatusReason', '')
@@ -94,7 +90,7 @@ def wait_for_change_set_creation(change_set_arn):
 def wait_for_stack_update(stack_name) -> bool:
     """Returns boolean indicating update success"""
 
-    stack = cf_resource.Stack(stack_name)
+    stack = _get_boto_resource("cloudformation").Stack(stack_name)
     while True:
         stack.reload()
         logger.debug(f"Stack '%s' status: %s", stack_name, stack.stack_status)
@@ -111,7 +107,7 @@ def wait_for_stack_update(stack_name) -> bool:
 
 def _s3_object_exists(bucket_name, key) -> bool:
     try:
-        s3_resource.Object(bucket_name, key).load()
+        _get_boto_resource("s3").Object(bucket_name, key).load()
         return True
     except ClientError:
         return False
@@ -119,7 +115,7 @@ def _s3_object_exists(bucket_name, key) -> bool:
 
 def _stack_exists(name) -> bool:
     try:
-        stack = cf_resource.Stack(name)
+        stack = _get_boto_resource("cloudformation").Stack(name)
 
         # Empty stack with state REVIEW_IN_PROGRESS appears when change set is
         # created. It is not deemed existing for the purposes of CREATE/UPDATE
@@ -131,3 +127,28 @@ def _stack_exists(name) -> bool:
 
 def _bytes_md5sum(binary):
     return str(hashlib.md5(binary).hexdigest())
+
+
+# Cache for boto3 client and resource objects.
+boto_resources = {}
+boto_clients = {}
+
+
+def _get_boto_resource(name):
+    if name not in boto_resources:
+        config.ensure_loaded()
+        resource = boto3.resource(name,
+                                  aws_access_key_id=config.AWS_ACCESS_KEY_ID,
+                                  aws_secret_access_key=config.AWS_ACCESS_KEY_SECRET)
+        boto_resources[name] = resource
+    return boto_resources[name]
+
+
+def _get_boto_client(name):
+    if name not in boto_clients:
+        config.ensure_loaded()
+        client = boto3.client(name,
+                              aws_access_key_id=config.AWS_ACCESS_KEY_ID,
+                              aws_secret_access_key=config.AWS_ACCESS_KEY_SECRET)
+        boto_clients[name] = client
+    return boto_clients[name]
